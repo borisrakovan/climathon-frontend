2dc47fdc79027ff5620d63fc371503b271b0fb05
Rakovan, Boris <rakovbor@fel.cvut.cz>
2021-10-23 09:59:48 +0200

    initial app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import ReactMapGL from "react-map-gl"
+// import "mapbox-gl/dist/mapbox-gl.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [viewport, setViewport] = React.useState({
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8,
+   })
+
+   return (
+      <div className="App">
+         <p>Hello world</p>
+         <div style={{ height: "500px" }}>
+            <ReactMapGL
+               {...viewport}
+               mapStyle="https://api.maptiler.com/maps/basic/style.json?key=w4vlPeqRikTQVaCu9Vf1"
+               width="100%"
+               height="100%"
+               onViewportChange={(viewport: any) => setViewport(viewport)}
+            />
+         </div>
+      </div>
+   )
 }
 
-export default App;
+export default App
