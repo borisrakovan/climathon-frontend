14d9410ba1946cfd9f77d15e04ffcf0a380189d0
Július Retzer <jul.retzer@gmail.com>
2021-10-23 12:10:14 +0200

    Tile Rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,73 @@
-import React from "react"
-import logo from "./logo.svg"
+import React, { useEffect } from "react"
 import "./App.css"
-import ReactMapGL from "react-map-gl"
-// import "mapbox-gl/dist/mapbox-gl.css"
+import ReactMapGL, { SVGOverlay } from "react-map-gl"
+
+const values = [
+   [0.2, 0.2342, 0.5, 0.34],
+   [0.2, 0.2342, 0.5, 0.34],
+   [0.2, 0.2342, 0.5, 0.34],
+   [0.2, 0.2342, 0.5, 0.34],
+]
+
+// type Data = number[][]
 
 function App() {
    const [viewport, setViewport] = React.useState({
-      latitude: 37.7577,
-      longitude: -122.4376,
-      zoom: 8,
+      latitude: 48.1486,
+      longitude: 17.1077,
+      zoom: 11,
    })
+
+   // const [data, setData] = React.useState<Data>([[]])
+
+   // useEffect(() => {
+   //    async function fetchData() {
+   //       const response = await fetch("http://10.137.4.31:5000/index")
+   //       const parsedResponse = await response.json()
+
+   //       setData(parsedResponse)
+   //    }
+
+   //    fetchData()
+   // }, [viewport])
+
+   console.log("viewport: ", viewport)
+
+   function redraw({ project, width, height }: any) {
+      console.log("heigth: ", height)
+      console.log("width: ", width)
+
+      const rows = values.length
+      console.log("rows: ", rows)
+      const columns = values[0].length
+      console.log("columns: ", columns)
+
+      const colWidth = width / columns
+      const rowHeight = height / rows
+
+      // const [cx, cy] = project([viewport.longitude, viewport.latitude])
+
+      return values.map((rows, indexRow) => {
+         return rows.map((column, indexCol) => {
+            console.log("column: ", column)
+
+            return (
+               <rect
+                  key={`${indexRow}-${indexCol}`}
+                  height={rowHeight}
+                  width={colWidth}
+                  x={0 + colWidth * indexRow}
+                  y={0 + rowHeight * indexCol}
+                  fill="blue"
+                  fill-opacity="0.4"
+               />
+            )
+         })
+      })
+   }
 
    return (
       <div className="App">
-         <p>Hello world</p>
          <div style={{ height: "500px" }}>
             <ReactMapGL
                {...viewport}
@@ -21,7 +75,12 @@ function App() {
                width="100%"
                height="100%"
                onViewportChange={(viewport: any) => setViewport(viewport)}
-            />
+            >
+               {/* <Source id="my-data" type="geojson" data={geojson}>
+                  <Layer {...layerStyle} />
+               </Source> */}
+               <SVGOverlay redraw={redraw} />
+            </ReactMapGL>
          </div>
       </div>
    )
